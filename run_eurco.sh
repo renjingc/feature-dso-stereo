007ace8a59b01546576d17bc3cfae91e3b317cad
renjingc <1184738634@qq.com>
2018-03-16 15:22:20 +0800

    add VINS IMU

--- run_eurco.sh
+++
@@ -1,8 +0,0 @@
-./build/bin/dso_dataset_euroc \
-files=/media/ren/99146341-07be-4601-9682-0539688db03f/我的数据集/图像公开数据集/V2_02_medium/mav0 \
-calib=/media/ren/99146341-07be-4601-9682-0539688db03f/我的数据集/图像公开数据集/V2_02_medium/mav0/calibLeft.txt \
-calibRight=/media/ren/99146341-07be-4601-9682-0539688db03f/我的数据集/图像公开数据集/V2_02_medium/mav0/calibRight.txt \
-vocab=/home/ren/work/dso_semantic/vocab/ORBvoc.bin \
-gtPath=\
-preset=2 \
-mode=1
--- src/FullSystem/CalibHessian.cpp
+++
@@ -1,6 +0,0 @@
-#include "CalibHessian.h"
-
-namespace fdso
-{
-  
-}
